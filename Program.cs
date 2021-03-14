e20b02912fc11c79feb0bde16832569fc512efea
SHINTA <shinta.0806@gmail.com>
2021-03-14 11:39:36 +0900

    TestAutoResetEvent 実装

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿// ============================================================================
+// 
+// AutoResetEvent と CountdownEvent の速度差比較
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+//
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestAutoResetEvent
 {
 	class Program
 	{
-		static void Main(string[] args)
+		public static async Task Main(String[] args)
 		{
-			Console.WriteLine("Hello World!");
+			const Int32 NUM_TRIALS = 100;
+
+			using AutoResetEvent autoResetEventMainToSub = new(false);
+			using AutoResetEvent autoResetEventSubToMain = new(false);
+			using CancellationTokenSource cancellationTokenSource = new();
+			TestAutoResetEvent testAutoResetEvent = new(autoResetEventMainToSub, autoResetEventSubToMain, cancellationTokenSource.Token);
+			Console.WriteLine("AutoResetEvent のテスト");
+			Int32 autoResetEventStart = Environment.TickCount;
+			Task autoResetEventTask = testAutoResetEvent.Invoke();
+			for (Int32 i = 0; i < NUM_TRIALS; i++)
+			{
+				autoResetEventMainToSub.Set();
+				autoResetEventSubToMain.WaitOne();
+			}
+			Console.WriteLine("所要時間：" + (Environment.TickCount - autoResetEventStart).ToString("#,0") + " [ms]");
+			cancellationTokenSource.Cancel();
+			autoResetEventMainToSub.Set();
+			await autoResetEventTask;
+			Console.WriteLine("カウント：" + testAutoResetEvent.Counter);
 		}
 	}
 }
---
+++ TestAutoResetEvent.cs
@@ -0,0 +1,89 @@
+﻿// ============================================================================
+// 
+// AutoResetEvent の速度測定
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+//
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestAutoResetEvent
+{
+	public class TestAutoResetEvent
+	{
+		// ====================================================================
+		// コンストラクター・デストラクター
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// コンストラクター
+		// --------------------------------------------------------------------
+		public TestAutoResetEvent(AutoResetEvent autoResetEventMainToSub, AutoResetEvent autoResetEventSubToMain, CancellationToken cancellationToken)
+		{
+			_autoResetEventMainToSub = autoResetEventMainToSub;
+			_autoResetEventToSubToMain = autoResetEventSubToMain;
+			_cancellationToken = cancellationToken;
+		}
+
+		// ====================================================================
+		// public プロパティー
+		// ====================================================================
+
+		public Int32 Counter { get; set; }
+
+		// ====================================================================
+		// public メンバー変数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// テスト開始
+		// --------------------------------------------------------------------
+		public Task Invoke()
+		{
+			return Task.Run(Test);
+		}
+
+		// ====================================================================
+		// private メンバー変数
+		// ====================================================================
+
+		// Main() からこちらへの通知
+		private AutoResetEvent _autoResetEventMainToSub;
+
+		// こちらから Main() への通知
+		private AutoResetEvent _autoResetEventToSubToMain;
+
+		// 中止用
+		private CancellationToken _cancellationToken;
+
+		// ====================================================================
+		// private メンバー変数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// テスト
+		// --------------------------------------------------------------------
+		private void Test()
+		{
+			while (true)
+			{
+				_autoResetEventMainToSub.WaitOne();
+				if (_cancellationToken.IsCancellationRequested)
+				{
+					return;
+				}
+
+				Counter++;
+				_autoResetEventToSubToMain.Set();
+			}
+		}
+	}
+}
